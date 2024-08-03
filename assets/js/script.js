cd05126aac750243c2ee5c95612d335497c8333e
kumarkapil08 <kapilkumar08012002@gmail.com>
2024-08-03 11:57:50 +0530

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+// Author = Kapi Kumar
+// File = Boscage furniture
+// version = 1.0
+
+// $('.hero-slider').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 1,
+//     adaptiveHeight: true,
+//     autoplay: true
+//   });
+
+  $('.hero-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="/assets/image/left-arrow.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="/assets/image/right-arrow.png"></button>'
+  });
+
+
+
+  $('.team-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="/assets/image/left-arrow.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="/assets/image/right-arrow.png"></button>'
+  });
